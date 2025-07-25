ca97a966ba76213ed1477ef733c5d93c8cfb0999
Азат Янбердин <azat@MacBook-Air.local>
2025-07-26 01:15:44 +0500

    add new http methods

--- handlers/tasks.py
+++ handlers/tasks.py
@@ -1,22 +1,60 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from fixtures import tasks as fixtures_tasks
+from schema.task import *
+from fastapi import status, HTTPException
+
 
 router = APIRouter(prefix="/task", tags=["task"])
 
-@router.get("/all")
-async def get_task():
-    return {"message": "ok"}
+
+@router.get(
+    "/all",
+    response_model=list[Task],
+)
+async def get_tasks():
+    return fixtures_tasks
 
 
-@router.get("/app")
-async def create_task():
-    return {"text": "app is working"}
+@router.get("/")
+async def create_task(body: Task):
+    return {"text": body}
 
 
-@router.post("/task/{task_id}")
-async def create_taskid():
-    return {"task_id": "id"}
+@router.post(
+    "/",
+    response_model=Task
+)
+async def create_task(task: Task):
+    fixtures_tasks.append(task)
+    return task
 
 
 @router.get("/tasks")
 async def tasks():
     return {''}
+
+
+@router.patch(
+    "/{task_id}",
+    response_model=Task
+)
+async def update_task(task_id: int, name: str):
+    for task in fixtures_tasks:
+        if task["id"] == task_id:
+            task["name"] = name
+            return task
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@router.delete(
+    "/{task_id}",
+    status_code=status.HTTP_204_NO_CONTENT
+)
+async def delete_task(task_id: int):
+    for index, task in enumerate(fixtures_tasks):
+        if task["id"] == task_id:
+            del fixtures_tasks[index]
+            return {"message": "task deleted"}
+        
+    return {"message": "task not found"}
